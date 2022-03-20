99086151d9e1a953ec82149922151376eacb3878
= <=>
2022-03-20 00:39:07 -0500

    Web api project

---
+++ Desktop/DevelopAPI/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace DevelopAPI.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+    }
+}
